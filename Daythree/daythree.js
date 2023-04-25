79155b58b554255b3bd4d75073d8940569d0e03f
Siddhika200 <124227297+Siddhika200@users.noreply.github.com>
2023-04-25 11:17:28 +0530

    Add files via upload

---
+++ Daythree/daythree.js
@@ -0,0 +1,26 @@
+/*alert("congratulations");
+alert("hello");
+alert(4+2);
+let num=14;
+   alert(num);
+
+const b=16;
+   alert(b);  
+var a="Thank you";
+   alert(a);*/
+// let a=13
+// a*=5
+// alert(a)   
+// let b=12==0
+// alert(b)
+// var x=13;
+// var y=12;
+// alert(x==y && x>y)
+//  if(x<y)
+//    alert("Siddhika")
+//  else
+//    alert("Ghule") 
+   
+// alert(typeof(true))
+// let age=prompt("Your age",100)
+//  alert("Your age is "+age)
---
+++ Daythree/intro.js
@@ -0,0 +1,7 @@
+function printlenghth(){
+    var str=document.getElementById("name").value;
+    var strlen=str.length;
+    document.getElementById("demo").innerHTML="Your score is :"+strlen
+}
+    
+
